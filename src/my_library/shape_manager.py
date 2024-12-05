f23314f8eb84a4ec4f98e547850315bc5d3387fa
Chen Kasirer <ckasirer@ethz.ch>
2024-12-05 16:33:25 +0100

    is this stupid

--- src/my_library/shape_manager.py
+++ src/my_library/shape_manager.py
@@ -2,46 +2,43 @@ from typing import List
 from compas.geometry import Sphere, Box, Vector, Point
 
 
-class Shape_Manager:
+class ShapeManager:
     def __init__(self):
-        self.SHAPES = []
+        self.shapes = []
 
     def add_sphere(self, center: Point, radius: float):
         """Add a sphere to the collection.
 
         Parameters
         ----------
-        center : Point
-            The center of the sphere.
-        radius : float
-            The radius of the sphere.
-
+        center
         """
         sphere = Sphere(center, radius)
-        self.SHAPES.append(sphere)
+        self.shapes.append(sphere)
 
     def add_box(self, corner: Point, width: float, height: float, depth: float):
-        """Add a box to the collection."""
+        """
+        Add a box to the collection.
+        """
         box = Box.from_corner_corner_height(
             corner, [corner[0] + width, corner[1] + height, corner[2]], depth
         )
-        self.SHAPES.append(box)
+        self.shapes.append(box)
 
     def total_volume(self):
-        """Compute the total volume of all shapes.
-        
-        Parameters:
-
-        Returns:
-            float: The total volume of all shapes.
-
+        """
+        Compute the total volume of all shapes.
         """
         # TODO: Implement this
 
     def largest_shape(self):
-        # Find the shape with the largest volume.
-        
+        """
+        Find the shape with the largest volume.
+        """
         # TODO: Implement this
 
     def move_shape(self, index, vector):
+        """
+        Move a shape by a given vector.
+        """
         # TODO: Implement this
